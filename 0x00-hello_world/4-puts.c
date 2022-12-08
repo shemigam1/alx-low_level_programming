75ed5b7dbb26e467860f4977fa1e7a1593e2b768
semilore <semilore@omotade-michaels.com>
2022-12-08 08:41:03 -0800

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
 
+ *
+
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char string[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(string);
-	return(0);
+  char string[] = "Programming is like building a multilingual puzzle\n";
+	puts (string);
+	return (0);
 }
