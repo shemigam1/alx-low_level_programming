695943aae1e14163e621318e37021cd8df58b260
semilore <semilore@omotade-michaels.com>
2022-12-08 08:35:47 -0800

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(){
-	char string[]="\"Programming is like building a multilingual puzzle\n";
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle\n"
 	puts(string);
 	return(0);
 }
