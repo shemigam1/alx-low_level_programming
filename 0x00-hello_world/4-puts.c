36bb94eb5721bd5efcb55a207f2fee88df373d60
semilore <semilore@omotade-michaels.com>
2022-12-08 08:44:02 -0800

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-  char string[] = "Programming is like building a multilingual puzzle\n";
-	puts (string);
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
