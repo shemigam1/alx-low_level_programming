3b96541d844a4b16ffa16bd5efde0d09c9bb78f5
semilore <semilore@omotade-michaels.com>
2022-12-09 06:41:24 -0800

    test

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is 0\n", n, a);
 }
 if (a < 6 && a != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, a);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 }
 return (0);
 }
