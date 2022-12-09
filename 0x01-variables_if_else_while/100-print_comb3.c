338fa743eb9bde77972be37369d1c6a08564a6e4
semilore <semilore@omotade-michaels.com>
2022-12-09 10:01:10 -0800

    test

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print unrecurring combinations of single digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ones;
+
+	int tens;
+
+	for (tens = 0; tens < 10; tens++)
+	{
+		for (ones = 0; ones < 10; ones++)
+		{
+			if (ones != tens || tens < ones)
+			{
+				putchar(tens);
+				putchar(ones);
+
+				if (ones != 9 && tens != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
