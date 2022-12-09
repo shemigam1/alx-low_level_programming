c3f8a1e451ba730e0ce16fdb7a69d3d788e31df7
semilore <semilore@omotade-michaels.com>
2022-12-09 06:53:14 -0800

    test

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -7,9 +9,8 @@
 int main(void)
 {
 char ap;
-for (ap = 'a'; ap < 'z'; ap++)
+for (ap = 'a';ap <= 'z'; ap++)
 {
-putchar("%c\n", ap);
-}
+putchar("%c", ap)
 return (0);
 }
