788b38a93520e50d10586464ca013e5b8ca4275a
semilore <semilore@omotade-michaels.com>
2022-12-13 10:11:01 -0800

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,14 @@
 
 int main(void)
 {
-	char arr[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
-	for (int i = 0; i < sizeof(arr)/sizeof(char); i++) 
-	{
-		_putchar(arr[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
