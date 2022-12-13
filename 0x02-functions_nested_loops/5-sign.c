38cfb79700ccbb85a7f35468929d82a2b91f8b3a
semilore <semilore@omotade-michaels.com>
2022-12-13 10:59:13 -0800

    test

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an int
+ * @c: character to be checked
+ *
+ * Return: 1 if c > 0, 0 if c = 0, -1 if c < 0
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{	
+		_putchar('0');
+		return (0);
+	}
+}
