7c368fb1b676daebbfecb482d9369d26108e359e
shemigam1 <97962478+shemigam1@users.noreply.github.com>
2022-12-13 23:16:18 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,86 @@
  * Return: void
  */
 
+// void print_times_table(int n)
+// {
+// 	int a = 0, rep, b;
+
+// 	if (n < 0 || n > 15)
+// 		return;
+
+// 	while (a <= n)
+// 	{
+// 		for (b = 0; b <= n; b++)
+// 		{
+// 			rep = a * b;
+// 			if(b == 0)
+// 				_putchar(rep + '0');
+// 			else if (rep < 10)
+// 			{
+// 				_putchar(' ');
+// 				_putchar(' ');
+// 				_putchar(rep + '0');
+// 			}
+// 			else if (rep < 100)
+// 			{
+// 				_putchar(' ');
+// 				_putchar((rep / 10) + '0');
+// 				_putchar((rep % 10) + '0');
+// 			}
+// 			else 
+// 			{
+// 				_putchar((rep / 100) + '0');
+// 				_putchar(((rep - 100) / 10) + '0');
+// 				_putchar((rep % 10) + '0');
+// 			}
+// 			if (b > n)
+// 			{
+// 				_putchar(',');
+// 				_putchar(' ');
+// 			}
+// 		}
+// 		a++;
+// 	}
+// }
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
+	int i, j, k;
 
-	if (n < 0 || n > 15)
-		return;
-
-	while (a <= n)
-	{
-		for (b = 0; b <= n; b++)
+	if (n >= 0 && n <= 15)
 		{
-			rep = a * b;
-			if(b == 0)
-				_putchar(rep + '0');
-			else if (rep < 10)
+			for (i = 0; i <= n; i++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(rep + '0');
+				for (j = 0; j <= n; j++)
+				{
+					k = j * i;
+					if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar((rep / 10) + '0');
-				_putchar((rep % 10) + '0');
-			}
-			else 
-			{
-				_putchar((rep / 100) + '0');
-				_putchar(((rep - 100) / 10) + '0');
-				_putchar((rep % 10) + '0');
-			}
-			if (b > n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		a++;
 	}
 }
+
