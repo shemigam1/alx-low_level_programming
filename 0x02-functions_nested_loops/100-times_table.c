328614dd7ec445d9594ab6c1e4085b49c9c853e8
semilore <semilore@omotade-michaels.com>
2022-12-13 13:22:44 -0800

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 			}
 			else 
 			{
-				_putchar((rep \ 100) + '0');
+				_putchar((rep / 100) + '0');
 				_putchar(((rep - 100) / 10) + '0');
 				_putchar((rep % 10) + '0');
 			}
