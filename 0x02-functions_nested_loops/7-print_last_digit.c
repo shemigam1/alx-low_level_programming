1f48571f24cc39f09abe4f572d60b5915c59a044
semilore <semilore@omotade-michaels.com>
2022-12-13 11:11:22 -0800

    test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns last digit
+ * @c: character
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+	
+	if (last < 0)
+		last *= -1;
+
+	_putchar(last + '0');
+
+	return (0);
+}
