e372e76f909c43f359314dec2d5a3b2be5cbfcfc
semilore <semilore@omotade-michaels.com>
2022-12-13 10:44:04 -0800

    test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks if a char islower
- * @c - character
+ * @c: character
  *
  * Return: 1 if success 0 if fail
  */
@@ -10,6 +10,6 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
+	else
 		return (0);
 }
