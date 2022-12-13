2d9425abecda4619f7f8a697bfbfdca0f83a86f7
semilore <semilore@omotade-michaels.com>
2022-12-13 13:21:42 -0800

    test

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - print 100 times table
+ * @n: index
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int a = 0, rep, b;
+
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			rep = a * b;
+			if(b == 0)
+				_putchar(rep + '0');
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(rep + '0');
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar((rep / 10) + '0');
+				_putchar((rep % 10) + '0');
+			}
+			else 
+			{
+				_putchar((rep \ 100) + '0');
+				_putchar(((rep - 100) / 10) + '0');
+				_putchar((rep % 10) + '0');
+			}
+			if (b > n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		a++
+	}
+}
