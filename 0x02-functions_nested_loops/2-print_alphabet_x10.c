7bf169fe5433c744566ca7427c711f32c92fe633
semilore <semilore@omotade-michaels.com>
2022-12-13 10:34:36 -0800

    test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints letters 10x times
+ */
+
+void print_alphabet_x10(void)
+{
+	int index;
+	char alp;
+
+	for (index = 0; index < 10; index++)
+	{
+		for (alp = 'a'; alp < 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+		_putchar('\n');
+	}
+}
