ce56312978c23444b5ec8167a9485ac15162cc37
semilore <semilore@omotade-michaels.com>
2022-12-15 15:40:47 -0800

    test

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,31 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b)
 	{
-		largest = a;
+		if (b > c)
+		{
+			largest = a;
+		}
+		else if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
 	}
-	else if (b > a && b > c)
+	else
 	{
-		largest = b;
-	}
-	else if (c > a && c > b)
-	{
-		largest = c;
+		if (c > b)
+		{
+			largest = c;
+		}
+		else
+		{
+			largest = b;
+		}
 	}
 
 	return (largest);
