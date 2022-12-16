1615d38150eee3718684c5b74a7d857916cbf4ed
semilore <semilore@omotade-michaels.com>
2022-12-15 16:24:01 -0800

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if param is uppercase char
+ * @c: input char
+ * Return: 1 if success, 0 if fail
+ */
+
+int _isupper(int c)
+{
+	if ( c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		retur (0);
+	}
+}
