0196714a585121ef7b56bdc55212e3eeb824ba54
semi <semilore@omotade-michaels.com>
2023-01-11 12:56:46 +0100

    test

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks if a char islower
+ * @c: character
+ *
+ * Return: 1 if success 0 if fail
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an int
+ * @c: character
+ *
+ * Return: absolute value
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
