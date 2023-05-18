0fc848ab4165610d3553da4af93e996d530c7e9e
semilore <semilore@omotade-michaels.com>
2023-05-18 20:12:11 +0100

    betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main: entry point
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("Size of a char: %i byte(s)", sizeof(char));
-	printf("Size of a int: %i byte(s)", sizeof(int));
-	printf("Size of a long int: %i byte(s)", sizeof(long int));
-	printf("Size of a long long: %i byte(s)", sizeof(long long));
-	printf("Size of a float: %i byte(s)", sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of a int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %i byte(s)\n", sizeof(long long));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
 	return (0);
 }
