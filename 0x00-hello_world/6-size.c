66842ddf5fdcfdbec751b094c56a825554afca5e
semilore <semilore@omotade-michaels.com>
2023-05-18 20:16:21 +0100

    betty documentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main: entry point
+ * main: entry point my file does stuff
  * Return: 0
  */
 
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of a int: %i byte(s)\n", sizeof(int));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %i byte(s)\n", sizeof(long long));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
 	return (0);
