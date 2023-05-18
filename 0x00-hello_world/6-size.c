38daf635328b2de15bb18cc9cac5da067a25c4ef
semilore <semilore@omotade-michaels.com>
2023-05-18 19:56:39 +0100

    Added a cfile that prints sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %i byte(s)", sizeof(char));
+	printf("Size of a int: %i byte(s)", sizeof(int));
+	printf("Size of a long int: %i byte(s)", sizeof(long int));
+	printf("Size of a long long: %i byte(s)", sizeof(long long));
+	printf("Size of a float: %i byte(s)", sizeof(float));
+
+	return (0);
+}
