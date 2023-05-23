990db0be2cd94ae8f4a74195b595daa83d916789
semilore <semilore@omotade-michaels.com>
2023-05-23 12:54:05 +0100

    Add: a cfile that checks if a char is an alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  * _isalpha - a function that checks for alphabetic characters
+ * @c: variable
  * Return: 1 on success 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
