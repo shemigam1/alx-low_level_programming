e6d32a5e2161f2252af41d5d9252eb8615888d9f
semilore <semilore@omotade-michaels.com>
2023-05-23 19:32:31 +0100

    Add: a cfile that prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			if (prod < 10)
+				_putchar(' ');
+			else
+				_putchar(prod / 10 + '0');
+			_putchar(prod % 10 + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
