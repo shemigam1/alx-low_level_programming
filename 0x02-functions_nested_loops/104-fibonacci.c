91e80c37a97f818cd0fd00045dd4a3a75e130ad4
semilore <semilore@omotade-michaels.com>
2023-05-23 22:45:11 +0100

    Add: a cfile that prints the first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int a, b, c, i;
+
+	a = 1;
+	b = 2;
+	printf("%lu, %lu, ", a, b);
+	for (i = 0; i < 95; i++)
+	{
+		c = a + b;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+	}
+	c = a + b;
+	printf("%lu\n", c);
+	return (0);
+}
