d1bfaf5dc04512f445cf48513660bb63ed3acb77
semilore <semilore@omotade-michaels.com>
2023-05-23 22:41:10 +0100

    Add: a cfile that prints the sum of even fibonacci numbers under 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,4 +7,20 @@
 
 int main(void)
 {
+	unsigned long long int a, b, c, i;
+
+	a = 0;
+	b = 1;
+	i = 0;
+	do {
+		c = a + b;
+		if (c % 2 == 0)
+			i += c;
+		a = b;
+		b = c;
+	} while (c <= 4000000);
+	printf("%llu\n", i);
+	return (0);
+}
+
 
