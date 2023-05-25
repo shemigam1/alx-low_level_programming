424dad6a606de0bfdf6f60fa2bc51041dcefd465
semilore <semilore@omotade-michaels.com>
2023-05-25 23:38:59 +0100

    add: 101 print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,25 +8,25 @@
 
 void print_number(int n)
 {
-	int div, num;
-
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
 
-	while (div <= n / 10)
-	{
-		div *= 10;
-	}
+	if (n == 0)
+		_putchar('0');
 
-	while (div > 0)
+	if (n > 1000)
 	{
-		num = n / div;
-		_putchar('0' + num);
-		n %= div;
-		div /= 10;
+		_putchar(n / 1000 + '0');
 	}
+	if (n > 100)
+		_putchar(n / 100 + '0');
+	if (n > 10)
+		_putchar(n / 10 + '0');
+
+	_putchar(n % 10 + '0');
+	_putchar('\n');
 }
 
