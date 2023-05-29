7f00987653d3c968682b90a84429f0b2eeab2d9c
semilore <semilore@omotade-michaels.com>
2023-05-29 20:19:12 +0100

    add: a cfile that reverses strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: arg
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char *i = s;
+	char *a = s;
+
+	while (*i != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	while (a < i)
+	{
+		char tmp = *a;
+
+		tmp = *a;
+		*a = *i;
+		*i = tmp;
+
+		a++;
+		i--;
+	}
+}	
