6cea04aeba7cc297b31ec281438018c945217f0d
semilore <semilore@omotade-michaels.com>
2023-05-29 21:35:16 +0100

    add: a cfile that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: arg
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	char *p = str;
+	int i = 0;
+	int half;
+
+	while (*p != '\0')
+	{
+		i++;
+		p++;
+	}
+	if (i % 2 == 0)
+		half = i / 2;
+	else
+		half = (i - 1) / 2;
+	p = str + half;
+
+	while (*p != '\0')
+	{
+		_putchar(*p);
+		p++;
+	}
+	_putchar('\n');
+}
