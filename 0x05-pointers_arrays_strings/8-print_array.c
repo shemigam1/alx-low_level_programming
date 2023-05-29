7597e8073b969e3e2ddb0b9fade065e91b77fe76
semilore <semilore@omotade-michaels.com>
2023-05-29 21:48:18 +0100

    add: a cfile that prints n elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+		printf(" \n");
 	for (i = 0; i < (n - 1); i++)
 	{
 		printf("%d, ", *(a + i));
 	}
 	printf("%d\n", *(a + (n - 1)));
-}
-				
+}	
