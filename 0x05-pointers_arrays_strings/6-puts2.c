a4171d977fc94a38e3ad4adc20c45f319af57678
semilore <semilore@omotade-michaels.com>
2023-05-29 21:14:02 +0100

    add: a cfile that prints every other char in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@
 
 void puts2(char *str)
 {
-	char *i = str;
+	int i, j = 0;
 
 	if (!str)
 		_putchar(' ');
@@ -17,11 +17,11 @@ void puts2(char *str)
 	{
 		i++;
 	}
-	while (*i != '\0')
+	while (j < i)
 	{
 		_putchar(*str);
 		str += 2;
 	}
-	_putchar('\n');
+
 }
 
