b93cbef5fe3444cd2ee7ded0a6db20f1e9c526b5
semilore <semilore@omotade-michaels.com>
2023-05-30 23:06:57 +0100

    fix: 8-printarray

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,12 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	if (n > 0)
+		
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", *(a + i));
-			if (i != n - 1)
-				printf(", ");
-		}
-		printf("\n");
+		printf("%d", *(a + i));
+		if (i != n - 1)
+			printf(", ");
 	}
+	printf("\n");
 }
