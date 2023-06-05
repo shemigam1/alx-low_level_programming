a5b6a5d1ee292bf1af2f5d4439571ad8e7211422
semilore <semilore@omotade-michaels.com>
2023-06-05 16:22:11 +0100

    Add: a cfile that finds the first occurence of a char in a str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a char in a string
+ * @s: ptr to string
+ * @c: char
+ * Return: ptr to char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
