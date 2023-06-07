f2061e44516e915a22c99ec975d0c81c5a52906f
semilore <semilore@omotade-michaels.com>
2023-06-07 11:08:23 +0100

    Add: cfile that checks if an int is prime recursively

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check_div - checks if num is divisible by div
+ * @num: int
+ * @div: int
+ * Return: int
+ */
+
+int check_div(int num, int div)
+{
+	if ((div * div) > num)
+		return (1);
+	if (num % div == 0)
+		return (0);
+	return (check_div(num, div + 1));
+}
+
+/**
+ * is_prime_number - checks if number is prime
+ * @n: int
+ * @Return: 0(success) 1(failure)
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_div(n, 2));
+}
