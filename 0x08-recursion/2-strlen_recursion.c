9c98852c65b5f4628e483c243c6311c9c8113ce5
semilore <semilore@omotade-michaels.com>
2023-06-07 10:08:28 +0100

    Add: cfile that returns the length of str recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lengthof the string
+ * @s: ptr to str
+ * Return: length of str
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return 1 + _strlen_recursion(s + 1);
+}	
