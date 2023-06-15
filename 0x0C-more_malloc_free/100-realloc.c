a3e2c44fc45ad3c8dfd5dd6a2f10209e7c331d6f
semilore <semilore@omotade-michaels.com>
2023-06-15 23:44:31 +0100

    add: a cfile that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,4 +11,36 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *newptr;
+	unsigned int i;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		return (NULL);
+
+	if (ptr != NULL)
+	{
+		char *src = (char *)ptr;
+		char *dest = (char *)newptr;
+
+		for (i = 0; i < new_size; i++)
+		{
+			dest[i] = src[i];
+		}
+	}
+	else
+	{
+		newptr = malloc(new_size);
+		return (newptr);
+	}
+	
+	free(ptr);
+	return (newptr);
 }
