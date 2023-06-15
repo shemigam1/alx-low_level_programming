51575db1a34633a4bb5fc86c4e11af10b2c4e7b3
semilore <semilore@omotade-michaels.com>
2023-06-15 22:29:58 +0100

    add: a cfile that creates an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function that creates an array of integers
+ * @min: int
+ * @max: int
+ * Return: ptr to int
+ */
+
+int *array_range(int min, int max)
+{
+	int range, i;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+	range = (max - min) + 1;
+	arr = malloc(sizeof(int) * range);
+	if (arr == NULL)
+		return NULL;
+	for (i = 0; i < range; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
