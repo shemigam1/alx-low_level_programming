189859bae2a7f56df0564d7ca8687d2819f67f1e
semilore <semilore@omotade-michaels.com>
2023-06-15 21:49:05 +0100

    add: a cfile thatconcatenates strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,72 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: str
+ * _strlen - returns length of string
+ * @s: arg
+ * Return: length of s
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - function that concatenates two strings
  * @s2: str
- * @n: size
- * Return: ptr to new str
+ * @s1: str
+ * @n: size of buffer
+ * Return: str
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j = 0;
 	char *str;
+	unsigned int s1len, s2len, a, b;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
 		s2 = "";
-
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (n < j)
-	{
-		str = malloc((i + n + 1) * sizeof(char));
-	}
-	else
-	{
-		str = malloc((i + j + 1) * sizeof(char));
-	}
+	s1len = _strlen(s1);
+	s2len = _strlen(s2);
+	str = malloc((s1len + n + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	strcpy(str, s1);
-	if (n < j)
-		strncat(str, s2, n);
+	a = 0;
+	while (s1[a] != 0)
+	{
+		str[a] = s1[a];
+		a++;
+	}
+	b = 0;
+	if (n > s2len)
+	{
+		while (s2[b] != '\0')
+		{
+			str[a] = s2[b];
+			a++;
+			b++;
+		}
+	}
 	else
-		strncat(str, s2, j);
+	{
+		unsigned int j;
+
+		for (j = 0; j < n; j++)
+		{
+			str[a] = s2[j];
+			a++;
+		}
+	}
+	str[a] = '\0';
 	return (str);
 }
