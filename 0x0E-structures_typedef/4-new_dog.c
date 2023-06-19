39d6e884375d3cd9cb9c60122429abef050a7911
semilore <semilore@omotade-michaels.com>
2023-06-19 18:48:43 +0100

    add: create new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _strcpy - copies string
+ * @dest: str
+ * @src: str
+ * Return: str
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ptr);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: str
+ * @age: float
+ * @owner: str
+ * Return: dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_c, *owner_c;
+	int i, j = 0;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	name_c = malloc(i * sizeof(char));
+	owner_c = malloc(j * sizeof(char));
+	if (name_c == NULL || owner_c == NULL)
+	{
+		free(new_dog);
+		free(name_c);
+		free(owner_c);
+		return (NULL);
+	}
+	_strcpy(name_c, name);
+	_strcpy(owner_c, owner);
+
+	new_dog->name = name_c;
+	new_dog->age = age;
+	new_dog->owner = owner_c;
+
+	return (new_dog);
+}
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-#include <stddef.h>
 
 /**
  * main - check the code
@@ -9,11 +8,9 @@
  */
 int main(void)
 {
-    struct dog my_dog;
+    dog_t *my_dog;
 
-    my_dog.name = NULL;
-    my_dog.age = -1.2;
-    my_dog.owner = NULL;
-    print_dog(&my_dog);
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
