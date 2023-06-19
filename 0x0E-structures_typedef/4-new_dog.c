4230f17d58c1a41e49d185bc7b83526f00cfae0a
semilore <semilore@omotade-michaels.com>
2023-06-19 19:04:11 +0100

    fix: create new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,30 +12,30 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *n_dog;
 	int i, j, a = 0;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	while (name[i++])
+		;
+	while (owner[j++])
+		;
+	n_dog = malloc(sizeof(dog_t));
+	if (n_dog == NULL)
 		return (NULL);
-	while (name[i++] != '\0')
-		;
-	while (owner[j++] != '\0')
-		;
-	new_dog->name = malloc(i * sizeof(new_dog->name));
-	if (new_dog == NULL)
+	n_dog->name = malloc(i * sizeof(n_dog->name));
+	if (n_dog == NULL)
 		return (NULL);
 	for (a = 0; a < i; a++)
 	{
-		new_dog->name[a] = name[a];
+		n_dog->name[a] = name[a];
 	}
-	new_dog->age = age;
-	new_dog->owner = malloc(j * sizeof(new_dog->owner));
-	if (new_dog == NULL)
+	n_dog->age = age;
+	n_dog->owner = malloc(j * sizeof(n_dog->owner));
+	if (n_dog == NULL)
 		return (NULL);
 	for (a = 0; a < j; a++)
 	{
-		new_dog->owner[a] = owner[a];
+		n_dog->owner[a] = owner[a];
 	}
-	return (new_dog);
+	return (n_dog);
 }
