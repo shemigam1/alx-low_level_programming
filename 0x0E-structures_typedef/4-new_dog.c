c4e2d0c6265daf9b9bf13e3b0d3874a59f9a96b5
semilore <semilore@omotade-michaels.com>
2023-06-19 19:00:20 +0100

    fix: create new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,26 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _strcpy - copies string
- * @dest: str
- * @src: str
- * Return: str
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	char *ptr = dest;
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (ptr);
-}
-/**
  * new_dog - creates a new dog
  * @name: str
  * @age: float
@@ -33,31 +13,29 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_c, *owner_c;
-	int i, j = 0;
+	int i, j, a = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
-	name_c = malloc(i * sizeof(char));
-	owner_c = malloc(j * sizeof(char));
-	if (name_c == NULL || owner_c == NULL)
-	{
-		free(new_dog);
-		free(name_c);
-		free(owner_c);
+	while (name[i++] != '\0')
+		;
+	while (owner[j++] != '\0')
+		;
+	new_dog->name = malloc(i * sizeof(new_dog->name));
+	if (new_dog == NULL)
 		return (NULL);
+	for (a = 0; a < i; a++)
+	{
+		new_dog->name[a] = name[a];
 	}
-	_strcpy(name_c, name);
-	_strcpy(owner_c, owner);
-
-	new_dog->name = name;
 	new_dog->age = age;
-	new_dog->owner = owner;
-
+	new_dog->owner = malloc(j * sizeof(new_dog->owner));
+	if (new_dog == NULL)
+		return (NULL);
+	for (a = 0; a < j; a++)
+	{
+		new_dog->owner[a] = owner[a];
+	}
 	return (new_dog);
 }
