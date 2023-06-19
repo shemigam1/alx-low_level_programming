79eb4ee90acd57522eceea594d617d442698d0f2
semilore <semilore@omotade-michaels.com>
2023-06-19 17:37:49 +0100

    add: initialize struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialized struct dog
+ * @d: struct
+ * @name: str
+ * @age: float
+ * @owner: str
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
