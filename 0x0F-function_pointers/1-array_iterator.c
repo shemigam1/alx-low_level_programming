69fab6fde0e2973bbc1a0433e5fc3d37da2e4030
semilore <semilore@omotade-michaels.com>
2023-06-21 21:27:03 +0100

    fix: add error handle

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,24 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * array_iterator - executes a function given
  * as a parameter on each element of an array
  * @array: array of int
  * @size: size_t
  * @action: function ptr
+ * Return: void
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL || action != NULL)
+	if (array == NULL || action == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			(*action)(array[i]);
-		}
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
 	}
 }
