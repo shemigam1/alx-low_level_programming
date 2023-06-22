5c67160f1c6bfca1f3cc5e69eef033746edb2526
semilore <semilore@omotade-michaels.com>
2023-06-22 15:21:40 +0100

    add: cfile that prints formatted string

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: char
+ * @n: char
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		unsigned int b;
+
+		b = va_arg(args, int);
+		printf("%d", b);
+		if (i != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/main.c
+++ 0x10-variadic_functions/main.c
@@ -6,13 +6,9 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
+    print_numbers(", ", 4, 0, 98, -1024, 402);
     return (0);
 }
