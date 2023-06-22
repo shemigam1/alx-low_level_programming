129f5cf80dd49c799ead1a116318450a4443f700
semilore <semilore@omotade-michaels.com>
2023-06-22 15:24:04 +0100

    fix: error handle

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
+	if (separator == NULL)
+	{
+		separator = "";
+	}
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
