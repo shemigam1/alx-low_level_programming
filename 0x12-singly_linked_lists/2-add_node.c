c047bc335c6fc31df67084e0e8b0866ed9bdf962
semilore <semilore@omotade-michaels.com>
2023-07-03 19:15:51 +0100

    add: function that add a node to the beginning of a singly linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to head of LL
+ * @head: head of LL
+ * @str: string
+ * Return: ptr to new head
+ *
+ * Description: (LL) Linked List
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head;
+
+	new_head = malloc(sizeof(list_t));
+	if (new_head == NULL)
+		return (NULL);
+	new_head->str = strdup(str);
+	new_head->len = strlen(str);
+	new_head->next = *head;
+	*head = new_head;
+	return (head);
+}
