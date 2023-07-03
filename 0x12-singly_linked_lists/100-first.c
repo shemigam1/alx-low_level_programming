27668ac2db3a5908e7b76fb9d4c4c6e27790a681
semilore <semilore@omotade-michaels.com>
2023-07-03 20:32:47 +0100

    add: prints before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,8 @@
  * Return: void
  */
 
-void first(void) __attribute__((constructor));
-
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
