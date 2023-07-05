4eabc1231df1bb06784c99ca7d43a82c4630a521
semilore <semilore@omotade-michaels.com>
2023-07-05 17:17:03 +0100

    add: frees a LL

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: ptr to head
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head->next);
+		free(head);
+		head = next;
+	}
+}
