7dd8b1f3cb1522f16653e60f11d0d1bf52b27b74
semilore <semilore@omotade-michaels.com>
2023-07-05 18:49:36 +0100

    add: inserts node at index idx

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert node at index
+ * @head: head node
+ * @idx: index
+ * @n: data
+ * Return: node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current;
+	listint_t *n_node;
+	unsigned int i = 0;
+
+
+	current = *head;
+	while (current != NULL)
+	{
+		if (i == idx)
+		{
+			n_node = malloc(sizeof(listint_t));
+			if (n_node == NULL)
+				return (NULL);
+			n_node->n = n;
+			n_node->next = current;
+		}
+		i++;
+		current = current->next;
+	}
+	return (n_node);
+}
