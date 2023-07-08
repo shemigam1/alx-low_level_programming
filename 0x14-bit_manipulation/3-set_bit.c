851c748c6fa5eb2c9faa73abf46759114a44b8a6
semilore <semilore@omotade-michaels.com>
2023-07-08 01:40:09 +0100

    add: set binary at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - sets vit at index
+ * @n: ptr to ulong
+ * @index: uint
+ * Return: int
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+		return (-1);
+	i = 1UL << index;
+	*n |= i;
+
+	rerurn (1);
+}
