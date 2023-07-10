6f8531044dc8a4ebf736b7c5557ad4b6fb77b594
semilore <semilore@omotade-michaels.com>
2023-07-10 21:48:41 +0100

    fix:

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,15 +14,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
+		bytesRead = 0;
 		while (*text_content != '\0')
+		{
 			bytesRead++;
+			text_content++;
+		}
 		bytesWrite = write(fd, text_content, bytesRead);
-		if (bytesWrite == -1)
+		if (bytesWrite == -1 || bytesWrite != bytesRead)
 		{
 			close(fd);
 			return (-1);
