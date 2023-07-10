443b3acddd83531972c4173e0af59194b1b1379a
semilore <semilore@omotade-michaels.com>
2023-07-10 21:21:13 +0100

    add: create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: filename
+ * @text_content: str
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytesRead;
+	ssize_t bytesWrite;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (*text_content != '\0')
+			bytesRead++;
+		bytesWrite = write(fd, text_content, bytesRead);
+		if (bytesWrite == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
