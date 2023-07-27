9d73403286ed3923608ee62a71ba9dfb28b1a0f1
semilore <semilore@omotade-michaels.com>
2023-07-27 21:14:43 +0100

    add:

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <fcntl.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
