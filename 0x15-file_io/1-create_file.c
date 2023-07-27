7df280e89550b24c92fc4615042fe916f1d9abc8
semilore <semilore@omotade-michaels.com>
2023-07-27 21:40:51 +0100

    add:

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - creates file
- * @filename: filename
- * @text_content: str
- * Return: 1 on success -1 on failure
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytesRead;
-	ssize_t bytesWrite;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content != NULL)
 	{
-		bytesRead = 0;
-		while (*text_content != '\0')
-		{
-			bytesRead++;
-			text_content++;
-		}
-		bytesWrite = write(fd, text_content, bytesRead);
-		if (bytesWrite == -1 || bytesWrite != bytesRead)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	else
-	{
-		bytesWrite = write(fd, "", 0);
 
-		if (bytesWrite == -1 || bytesWrite != 0)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
 	close(fd);
+
 	return (1);
 }
