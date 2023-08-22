d6cf0d42ba22c2da063f211f336e5978970f2fe9
semilore <semilore@omotade-michaels.com>
2023-08-22 15:42:57 +0100

    add: adds node to beginning of dll

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,11 +16,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = (*head);
-	if (*head != NULL)
+	new_node->next = NULL;
+	if ((*head) == NULL)
 	{
-		(*head)->prev = new_node;
-		*head = new_node;
+		(*head) = new_node;
+		return (new_node);
 	}
+	(*head)->prev = new_node;
+	new_node->next = (*head);
+	(*head) = new_node;
 	return (new_node);
 }
