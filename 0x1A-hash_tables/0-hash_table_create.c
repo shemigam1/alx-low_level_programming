b708c0a5f8f82b5108fbc712e2789cb403e92272
semilore <semilore@omotade-michaels.com>
2023-10-06 13:31:15 +0100

    add: create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: unsigned long int
+ * Return: hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	unsigned long int i;
+
+	if (size < 1)
+		return NULL;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return NULL;
+	table->size = size;
+	table->array = malloc(sizeof(hash_table_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
